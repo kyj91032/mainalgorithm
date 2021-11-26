3e2311ff714c002dd91a072c2252cda133e6aea5
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2021-11-26 19:27:06 +0900

    Create Selection sort.c

---
+++ Selection sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define swap(type,x,y) do{type t=x; x=y; y=t;} while(0)
+
+int selection(int a[], int n)
+{
+  int i, j;
+  for(i=0; i < n-1; i++) {
+    int min = j;
+    for(j = i+1; j < n; j++){
+      if(a[j] < a[min])
+        min = j;
+    }
+    swap(int, a[i], a[min]);
+  }
+}
