570d3ead277546490b37a4cd8c7220bb839ac831
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-01-09 16:20:29 +0900

    Update breadth_first_search.py

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -1,19 +1,21 @@
 #BFS의 동작 과정과 코드
 '''
-구체적인 동작 과정: 인접 노드들을 먼저 방문하면서 전체를 점령함 (큐).
-1. 탐색 시작 노드를 큐에 삽입하고 방문 처리를 한다.
-2. 큐에서 노드를 꺼내 해당 노드의 인접 노드 중에서 방문하지 않은 노드를 모두 큐에 삽입하고 방문 처리를 한다.
+구체적인 탐색 과정: 인접 노드들을 우선으로 기억하면서 전체를 점령함 (queue).
+
+1. 탐색 시작 노드를 큐에 삽입하고 기억 처리를 한다.(enque와 기억처리)
+2. 큐에서 노드를 꺼낸다.(deque)
+3. 해당 노드의 인접 노드 중에서 기억하지 않은 노드를 모두 큐에 삽입하고 기억 처리를 한다.(인접노드 enque와 기억처리)
 '''
-def bfs(graph, start, visited): # 너비 우선 탐색 함수 정의. 매개변수(그래프 graph, 노드 포인터 v, 방문 여부 기억공간 visited[v] = 0 or 1)
-  queue = deque([start]) # 1. 탐색 시작 노드를 큐에 삽입하고
-  visited[start] = True # 방문 처리를 한다.
+def bfs(graph, start, visited): # 너비 우선 탐색 함수 정의. 매개변수(그래프 graph, 노드 포인터 v, 기억 공간 visited[v] = 0 or 1)
+  queue = deque([start]) # 1. 탐색 시작 노드를 큐에 enque하고
+  visited[start] = True # 기억 처리를 한다.
   while queue:
-    v = queue.popleft() # 2. 큐에서 노드를 pop하고
+    v = queue.popleft() # 2. 큐에서 노드를 deque하고
     printf(v, end = ' ')
     for i in graph[v]: # 해당 노드의 인접 노드들 중에서
-      if not visited[i]: # 방문하지 않은 노드를
-        queue.append(i) # 큐에 삽입하고
-        visited[i] = True # 방문 처리를 한다.
+      if not visited[i]: # 기억하지 않은 노드를
+        queue.append(i) # 큐에 enque하고
+        visited[i] = True # 기억 처리를 한다.
 
 
 graph = [ #인접 리스트 방식(?)으로 그래프 표현.
@@ -44,12 +46,12 @@ for i in range(n):
 dx = [-1, 1, 0, 0] # 인접 노드 포인터를 얻기 위함.
 dy = [0, 0, -1, 1]
 
-def bfs(x, y):
+def bfs(x, y): # bfs의 매개변수. (graph(한 개로 정해져 있으니 받을 필요 없음), 노드 포인터 (x, y), 방문처리 (0, 1+ 최단거리기록))
 	queue = deque()
-	queue.append((x, y))
+	queue.append((x, y)) # enque와 기억처리
 	while queue:
-		x, y = queue.popleft() # 
-		for i in range(4): # 현재 위치에서 네 방향 
+		x, y = queue.popleft() # duque
+		for i in range(4): # 현재 위치에서 네 방향 (인접노드)
 			nx = x + dx[i]
 			ny = y + dy[i]
 			if nx < 0 or ny < 0 or nx >= n or ny >= m:
@@ -57,8 +59,8 @@ def bfs(x, y):
 			if graph[nx][ny] == 0:
 				continue
 			if graph[nx][ny] == 1:
-				graph[nx][ny] = graph[x][y] + 1 # 최단 거리 기록
-				queue.append((nx, ny))
-	return graph[n - 1][m - 1]
+				queue.append((nx, ny)) # 인접노드 enque와
+				graph[nx][ny] = graph[x][y] + 1 # 기억처리(최단거리 기록) **따로 변수 설정하면 중복되서 값이 더 크게 나옴.
+	return graph[n - 1][m - 1] # 최단거리 리턴.
 
 print(bfs(0, 0))
