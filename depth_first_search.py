ae913314eea005692a2ef85d24eb30e0be0f42e8
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-01-12 22:34:58 +0900

    Update depth_first_search.py

--- depth_first_search.py
+++ depth_first_search.py
@@ -8,11 +8,12 @@
 '''
 #(재귀)
 # dfs 함수 정의. 매개변수(그래프 graph, 노드 포인터 v, 기억 공간 visited[v] = 0 or 1)
-def dfs(graph, v, visited): # 1. 탐색 시작 노드 v를 스택에 삽입하고,
-  visited[v] = True # 기억처리를 한다.
+def dfs(graph, v, visited): # 1. 탐색 시작 노드 v를 스택에 삽입하고
+			    # (stack.append(v)가 시작으로서 탐색시작 노드v의 push이어야하지만, 스택 자료구조이므로 재귀함수 알고리즘으로 바꾸면 함수의 호출 자체가 push가 된다.)
+  visited[v] = True # 기억처리를 한다. (기억처리가 되어있다면 push후 바로 pop되는 것)
   print(v, end = ' ')
   for i in graph[v]: # 2. 최상단 노드 v의 인접 노드들 graph[v]에서
-    if not visited[i]: # 기억하지 않은 인접 노드가 있으면 
+    if not visited[i]: # 기억하지 않은 인접 노드가 있으면
       dfs(graph, i, visited) # 그 인접 노드를 스택에 넣고 기억처리 한다(재귀).
 			     # 3. 인접 노드들 중 기억하지 않은 인접노드가 없을 시 함수는 끝나므로(=최상단 스택이 pop) 최후방 함수의 호출이 종료.
 
@@ -40,11 +41,10 @@ graph = []
 for i in range(N):
 	graph.append(list(map(int, input()))) # **map은 iteralbe의 요소마다 지정된 함수를 적용시켜준다. list(string)은 각 문자가 요소가 되는 리스트로 변환된다.
 
-def dfs(x, y): # dfs의 매개변수. (graph(한 개로 정해져 있으니 매개변수로 넣지 않아도 됨), 노드 포인터 (x, y), 기억처리(= 얼음 여부 0, 1))
+def dfs(x, y): # dfs의 매개변수. (graph(한 개로 정해져 있으니 매개변수로 넣지 않아도 됨), 노드 포인터 (x, y), 기억처리(= 얼음 여부 0, 1)) # push와
 	if x <= -1 or x >= n or y <= -1 or y >= m:
 		return False
-	if graph[x][y] == 0: # 문제 특성 상(기억 처리 되어있는 노드가 처음부터 있음) 재귀를 위해 첫 노드부터 기억 여부 판단한 것. 원래는 바로 기억처리.
-			     # push는 했는데 기억처리가 되어있어서 바로 pop..
+	if graph[x][y] == 0:
 		graph[x][y] = 1 # 기억처리
 		dfs(x - 1, y) # 인접노드 push와 기억처리
 		dfs(x, y - 1)
